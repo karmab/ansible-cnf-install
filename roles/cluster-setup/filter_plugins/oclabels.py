c3443d98844baeb1993934c1424cfb0baf8fe8ca
karmab <karimboumedhel@gmail.com>
2020-05-19 17:03:55 +0200

    overwrite labels

--- roles/cluster-setup/filter_plugins/oclabels.py
+++ roles/cluster-setup/filter_plugins/oclabels.py
@@ -7,6 +7,6 @@ class FilterModule(object):
         for entry in hostvar:
             if 'labels' in hostvar[entry] and cluster_group in hostvar[entry]['group_names']:
                 labels = ' '.join(['%s=""' % label for label in hostvar[entry]['labels']])
-                label = "oc label node %s %s" % (entry, labels)
+                label = "oc label node %s %s --overwrite" % (entry, labels)
                 results.append(label)
         return results
